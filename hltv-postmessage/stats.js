05378459c49a317d072b2cbb1c4746de7d62711c
nils <nils@nils>
2024-02-17 11:33:48 +0100

    merge

--- hltv-postmessage/stats.js
+++ hltv-postmessage/stats.js
@@ -1,5 +1,8 @@
 const HLTV_STATS_BASE = 'https://www.hltv.org/stats';
 const HLTV_MONEY_BASE = 'https://www.hltv.org/fantasy';
+const HLTV_SEARCH_BASE = 'https://www.hltv.org/search';
+
+const HLTV_STATS_URL_REGEX = /a href="\/events\/(?<eventStatsId>.*?)\/(?<eventStatsName>.*?)"/gm;
 
 // wait for ~300ms between requests to prevent timeouts from hltv.org
 var holdoffUntil = new Date();
@@ -10,7 +13,7 @@ function postToOpener(msg) {
   }
 }
 
-function checkHoldoff() {
+function checkHoldoff(cmd) {
   if (new Date() < holdoffUntil) {
     postToOpener({'cmd': cmd.cmd, 'status': 'holdoff'});
     return false;
@@ -24,29 +27,52 @@ function statsRequestHandler(event) {
     console.info(`received command`);
     console.info(cmd);
 
+    if (!checkHoldoff(cmd)) { return }
+
     if (cmd.cmd == 'getPlayerStats') {
-      if (!checkHoldoff()) { return }
       let url = `${HLTV_STATS_BASE}/players?${new URLSearchParams(cmd.filter).toString()}`;
       console.debug(`fetching ${url}`);
-      fetch(`${HLTV_STATS_BASE}/players?${new URLSearchParams(cmd.filter).toString()}`)
-      .then((resp) => resp.text())
+      fetch(url).then((resp) => resp.text())
       .then((html) => postToOpener({
         'cmd': cmd.cmd,
         'status': 'ok',
         'filter': cmd.filter,
         'playerStatsHtml': html
       }));
+
     } else if (cmd.cmd == 'getMoneyStats') {
-      if (!checkHoldoff()) { return }
-      console.debug(`${HLTV_MONEY_BASE}/${cmd.fantasyInfo.eventId}/leagues/${cmd.fantasyInfo.leagueId}/join/json`);
-      fetch(`${HLTV_MONEY_BASE}/${cmd.fantasyInfo.eventId}/leagues/${cmd.fantasyInfo.leagueId}/join/json`)
-      .then((resp) => resp.json())
+      let url = `${HLTV_MONEY_BASE}/${cmd.fantasyInfo.eventId}/leagues/${cmd.fantasyInfo.leagueId}/join/json`;
+      console.debug(`fetching ${url}`);
+      fetch(url).then((resp) => resp.json())
       .then((data) => postToOpener({
         'cmd': cmd.cmd,
         'status': 'ok',
         'fantasyInfo': cmd.fantasyInfo,
         'playerStatsJson': data['moneyDraftData']['teams']
       }));
+
+    } else if (cmd.cmd == 'getSearch') {
+      let url = `${HLTV_SEARCH_BASE}?${new URLSearchParams(cmd.query).toString()}`;
+      console.debug(`fetching ${url}`);
+      fetch(url).then((resp) => resp.json())
+      .then((data) => postToOpener({
+        'cmd': cmd.cmd,
+        'status': 'ok',
+        'query': cmd.query,
+        'json': data
+      }));
+
+    } else if (cmd.cmd == 'findStatsUrls') {
+      postToOpener({
+        'cmd': cmd.cmd,
+        'status': 'ok',
+        'url': window.location.href,
+        'json': [...document.querySelector('html')
+          .innerHTML
+          .matchAll(HLTV_STATS_URL_REGEX)]
+          .map((m) => m.groups)
+      });
+
     } else {
       console.warn(`unknown cmd ${cmd.cmd}`);
     }
@@ -55,7 +81,7 @@ function statsRequestHandler(event) {
 if (!!window.opener) {
     window.addEventListener("message", statsRequestHandler, false);
     console.info('waiting for requests from opener');
-    postToOpener('ping');
+    postToOpener({'cmd': 'ping', 'status': 'pong'});
 } else {
     console.debug('no opener found, not adding handler!');
 }
